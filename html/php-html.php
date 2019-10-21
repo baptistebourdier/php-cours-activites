a105ab38cd54a1a430f86a672b6d67f2b205d501
Baptiste Bourdier <mrbou95410@gmail.com>
2019-10-21 18:15:30 +0200

    fin des exos html

--- html/php-html.php
+++ html/php-html.php
@@ -12,7 +12,11 @@
         $students = ['Hulk', 'Iron Man', 'Wonder Woman', 'Black Widow', 'Malicia'];
      ?>
      <ul>
-       <?php //display the students here ?>
+       <?php 
+        foreach($students as $nom){
+          echo $nom . " , ";
+        }
+       ?>
      </ul>
      <hr>
      <h1>Date du jour</h1>
@@ -20,24 +24,27 @@
 
        <!-- Instructions : Créer la liste de jour (en chiffres), de mois (en chiffres) et d'année en PHP. -->
        <label for="day">Day</label>
-       <select  name="day"><?php //list of day ?></select>
+       <select  name="day"><?php for($i=1 ; $i <= 31 ; $i++){echo "<option value =" . $i . " >$i</option>";} ?></select>
        <label for="month">Month</label>
-       <select  name="month"><?php //list of month ?></select>
+       <select  name="month"><?php for($j=1 ; $j <= 12 ; $j++){echo "<option value =" . $j . " >$j</option>";} ?></select>
        <label for="year">Year</label>
-       <select  name="year"><?php //list of year ?></select>
+       <select  name="year"><?php for($k=2017; $k <= 2025 ; $k++){echo "<option value =" . $k . " >$k</option>";} ?></select>
      </form>
      <hr>
+     <?php 
+     if(isset($_GET['sexe'])){
+       if($_GET['sexe'] == 'fille'){
+         echo "<p> Je suis une fille </p>";
+       } elseif($_GET['sexe'] == 'garçon'){
+         echo "<p> Je suis un garçon </p>";
+       }else{
+         echo "Je suis indéfini";
+       }
+     }
+     
+     ?>
      <!-- Instruction : Afficher ce bloc que si dans l'URL il y'a une variable sexe et que ça valeur vaut "fille" -->
-     <p>
-       Je suis une fille
-     </p>
      <!-- Instruction : Afficher ce bloc que si dans l'URL il y'a une variable sexe et que ça valeur vaut "garçon" -->
-     <p>
-       Je suis un garçon
-     </p>
      <!-- Instruction : Afficher ce bloc dans les autres cas -->
-     <p>
-       Je suis indéfini
-     </p>
   </body>
 </html>
